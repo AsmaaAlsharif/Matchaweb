7e6fb08848b3fa802c2e37b169d28bedec6d9ba0
Asmaa Alsharif <asmaaalsharif23@gmail.com>
2025-01-12 17:41:38 -0700

    Linking about me and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css'
+
+function Home() {
+  return (
+    <div>
+      <h1>Matcha Blog</h1>
+      <p>This is the home page.</p>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>This is the about me page.</p>
+    </div>
+  );
+}
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Me</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
